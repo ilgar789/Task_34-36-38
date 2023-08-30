7305876ea0e6f908bc26aaa8ea03f996ed4a90e9
ilgar789 <ilgar789@gmail.com>
2023-08-30 12:33:09 +0400

    Add_Task_Odd_Index_Sum

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+
+            // Создаем массив и заполняем его случайными числами
+            int[] array = new int[4]; // Можете изменить размер массива по вашему желанию
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+
+            // Выводим массив для проверки
+            Console.WriteLine("Массив случайных чисел:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            // Находим сумму элементов на нечетных позициях
+            int oddPositionSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 != 0) // Если индекс нечетный
+                {
+                    oddPositionSum += array[i];
+                }
+            }
+
+            // Выводим сумму элементов на нечетных позициях
+            Console.WriteLine("Сумма элементов на нечетных позициях: " + oddPositionSum);
+        
